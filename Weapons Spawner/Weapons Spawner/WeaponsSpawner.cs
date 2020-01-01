f68263084c1b526da358d13e29be43d2258535b8
matthewvbro101 <masteryblackcraft@gmail.com>
2020-01-01 11:52:54 -0800

    init

---
+++ Weapons Spawner/Weapons Spawner/WeaponsSpawner.cs
@@ -0,0 +1,30 @@
+﻿using GTA;
+using System;
+using System.Windows.Forms;
+
+public class WeaponsSpawner : Script
+{
+    public WeaponsSpawner()
+    {
+        Tick += OnTick();
+        KeyDown += OnKeyDown();
+        KeyUp += OnKeyUp();
+
+        Interval = 10;
+    }
+
+    void OnTick(object sender, EventArgs e)
+    {
+
+    }
+
+    void OnKeyDown(object sender, KeyEventArgs e)
+    {
+
+    }
+
+    void OnKeyUp(object sender, KeyEventArgs e)
+    {
+
+    }
+}
